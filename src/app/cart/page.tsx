64a677d08e25163dab7149481a49ec48370b78d6
Liam <liam.alton@signaturegroup.com.au>
2024-12-06 22:41:44 +1100

    created cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,13 +1,37 @@
 "use client"
 import { useCart } from "@/providers/CartProvider";
 import { Product } from "@/util/getProducts/type";
-import { CSSProperties, useEffect, useState } from "react";
+import { CSSProperties, DetailedHTMLProps, TdHTMLAttributes, ThHTMLAttributes, useEffect, useState } from "react";
 import Image from 'next/image'
-import Button from "@/components/Button";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 const imageStyle: CSSProperties = {
   height: 50,
   objectFit: 'contain',
+}
+
+function TableData({children, className, ...restProps}: DetailedHTMLProps<TdHTMLAttributes<HTMLTableDataCellElement>, HTMLTableDataCellElement>) {
+  className = `p-2 ${className ? className : ''}`
+
+  return (
+    <td {...restProps} className={className}>
+      {children}
+    </td>
+  )
+}
+
+function TableHeader({children, className, ...restProps}: DetailedHTMLProps<ThHTMLAttributes<HTMLTableDataCellElement>, HTMLTableDataCellElement>) {
+  className = `p-2 ${className ? className : ''}`
+
+  return (
+    <th {...restProps} className={className}>
+      {children}
+    </th>
+  )
+}
+
+function financial(x: string | number) {
+  return Number(x).toFixed(2);
 }
 
 export default function CartPage() {
@@ -25,28 +49,68 @@ export default function CartPage() {
   console.log('Cart Products:', products);
   console.log('Cart State:', state);
 
+  const getQuantity = (product: Product) => {
+    return state.items.find(item => item.id === product.id)?.quantity || 0;
+  }
+
   return (
     <div className="flex justify-center">
       <div className="flex flex-wrap justify-center flex-row gap-4 w-fit p-4 bg-white shadow-sm border border-slate-100 rounded">
-        {products.map(product => {
-          const quantity = state.items.find(item => item.id === product.id)?.quantity;
-
-          return (
-            <div key={product.id} className="flex rounded flex-row items-center gap-4 border border-slate-400 w-full pr-4">
-              <div className=" p-4 bg-white flex justify-center align-middle">
-                <Image style={imageStyle} src={product.image} alt={product.title} width={50} height={50} />
-              </div>
-              <h3 className="text-lg max-w-60 line-clamp-1">{product.title}</h3>
-              <p>${product.price}</p>
-              <div>
-                <Button onClick={() => dispatch({ type: 'ADD_ITEM', payload: { id: product.id, quantity: 1 } })}></Button>
-                <p>{state.items.find(item => item.id === product.id)?.quantity}</p>
-                <Button onClick={() => dispatch({ type: 'REMOVE_ITEM', payload: { id: product.id, quantity: 1} })}></Button>
-              </div>
-              {quantity && <p>Subtotal: ${Number(product.price) * quantity}</p>}
-            </div>
-          )
-    })}
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <TableHeader>Item</TableHeader>
+              <TableHeader>Price</TableHeader>
+              <TableHeader>Quantity</TableHeader>
+              <TableHeader>Sub Total</TableHeader>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map(product => {
+              const quantity = getQuantity(product);
+              return (
+                <tr key={product.id}>
+                  <TableData>
+                    <div className="flex flex-row gap-2 justify-center items-center">
+                      <div className="bg-white flex justify-center align-middle">
+                        <Image style={imageStyle} src={product.image} alt={product.title} width={50} height={50} />
+                      </div>
+                      <h3 className="max-sm:w-0 text-lg max-w-60 line-clamp-1">{product.title}</h3>
+                    </div>
+                  </TableData>
+                  <TableData>
+                    <p>${financial(product.price)}</p>
+                  </TableData>
+                  <TableData>
+                    <div className="flex flex-col justify-center items-center">
+                      <button onClick={() => dispatch({ type: 'ADD_ITEM', payload: { id: product.id, quantity: 1 } })}>
+                        <FaChevronUp color='#334155'/>
+                      </button>
+                      <p>{state.items.find(item => item.id === product.id)?.quantity}</p>
+                      <button onClick={() => dispatch({ type: 'REMOVE_ITEM', payload: { id: product.id, quantity: 1} })}>
+                        <FaChevronDown color='#334155'/>
+                      </button>
+                    </div>
+                  </TableData>
+                  <TableData>
+                    {quantity && <p>${financial(Number(product.price) * quantity)}</p>}
+                  </TableData>
+                </tr>
+              )
+            })}
+            <tr>
+              <TableData colSpan={4}>
+                <div className="flex flex-row justify-end items-center gap-2">
+                  <h3 className="text-lg">Total</h3>
+                  <p>${financial(products.reduce((acc, product) => {
+                  const quantity = getQuantity(product);
+                  return acc + (Number(product.price) * quantity);
+                  }, 0))}</p>
+                </div>
+              </TableData>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   )
